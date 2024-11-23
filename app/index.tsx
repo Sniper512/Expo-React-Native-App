874130aabdc1154e304fc3c968abddadb9cbee7c
Muhammad Jafar <103287122+Sniper512@users.noreply.github.com>
2024-11-23 23:35:20 +0500

    start

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      className="flex justify-center items-center bg-black w-full h-full"
+    >
+      <Text className="text-white">Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
